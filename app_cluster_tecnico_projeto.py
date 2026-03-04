af0a563a6eb56f297a4e5565f8cfc55c457493be
WeliNog <nogueiraswns@gmail.com>
2026-03-04 15:48:42 -0300

    Refactor app_cluster_tecnico_projeto.py for clarity

--- app_cluster_tecnico_projeto.py
+++ app_cluster_tecnico_projeto.py
@@ -175,8 +175,8 @@ with col4:
 # ---------------- ABAS ---------------- #
 tab1, tab2, tab3, tab4, tab5 = st.tabs([
     "Heatmap",
-    "Heatmap Cluster SP)",
-    "Áreas 100%",
+    "Heatmap (Cluster SP)",
+    "Áreas 100% em um Cluster",
     "Análise por Cluster",
     "NODES Não Encontrados"
 ])
@@ -523,6 +523,7 @@ with tab5:
         tabela_abs,
         use_container_width=True
     )
+
 
 
 
